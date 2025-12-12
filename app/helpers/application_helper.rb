2049ed70224e1b16be7f8324ceb6fc2f3cd21a02
DJP1012878 <70236671+DJPetika@users.noreply.github.com>
2025-12-12 11:19:55 -0600

    ran out of my elotted 2 hours trying to fix the heroku deployment. So my change is pretty simple. I changes all the colors to magenta! You're welcome!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,23 +1,23 @@
 module ApplicationHelper
     def spotify_colors
     [
-      "#1DB954", # Spotify Green
-      "#004D40", # Dark Teal
-      "#1ED760", # Bright Mint
-      "#B3FF66", # Lime Glow
-      "#93E9BE", # Seafoam
-      "#D8FFB6", # Pale Lime
+      "#b91dae", # Spotify Pink
+      "#E75480", # Dark Pink
+      "#FF08E8", # Bright Magenta
+      "#E4287C", # Pink Lemonade
+      "#E7ACCE", # Pink Pearl
+      "#FDC1C5", # Pale Rose
       "#A7A7A7", # Cool Gray
       "#535353", # Medium Gray
-      "#6DCB8B", # Faded Spring
-      "#8CCB5E", # Olive Lime
+      "#c56dcb", # Faded plum
+      "#cb5ebb", # Kirby Foot
       "#2ECC71", # Soft Leaf
       "#A8E6CF", # Light Mint
       "#E0E0E0", # Light Gray Neutral
       "#C2FFD9", # Mint Frost
-      "#9DE69E", # Soft Green
-      "#B2DFDB", # Pale Teal Gray
-      "#00B36B", # Deep Emerald
+      "#cc2e55", # Soft Cherry
+      "#dfcab2", # Pale Golden Gray
+      "#b30018", # Deep Rouge
       "#F5F5F5", # Off White
       "#191414"  # Spotify Dark Base
     ]
